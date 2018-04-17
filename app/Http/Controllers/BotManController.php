ef3000a82faa3c5da7f6c18416fa143a5f3faa5b
Florian Wartner <f.wartner@icloud.com>
2018-04-17 11:09:15 +0200

    Changes

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use BotMan\BotMan\Middleware\ApiAi;
 use BotMan\BotMan\BotMan;
 use Illuminate\Http\Request;
-use App\Conversations\ExampleConversation;
 
 class BotManController extends Controller
 {
@@ -17,22 +16,5 @@ class BotManController extends Controller
         $botman = app('botman');
 
         $botman->listen();
-    }
-
-    /**
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    public function tinker()
-    {
-        return view('tinker');
-    }
-
-    /**
-     * Loaded through routes/botman.php
-     * @param  BotMan $bot
-     */
-    public function startConversation(BotMan $bot)
-    {
-        $bot->startConversation(new ExampleConversation());
     }
 }
